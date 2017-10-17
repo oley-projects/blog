89ee9193f47503d1e52c4b2f0af80d4ae82f40b5
Oley-Projects <oley.for.git@gmail.com>
2017-10-17 15:52:30 +0300

    Implement articles#show, fixed bug

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,12 +1,19 @@
 class ArticlesController < ApplicationController
 
+  def show
+    @article = Article.find(params[:id])
+  end
+
   def new
   end
 
   def create
     @article = Article.new(article_params)
+
     if @article.valid?
       @article.save
+      redirect_to @article
+
     else
       render action: 'new'
     end
