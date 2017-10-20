04dae8ee9671acef480483417cf10e787c0696b6
Oley-Projects <oley.for.git@gmail.com>
2017-10-21 00:09:03 +0300

    Add article comments

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,9 @@ Rails.application.routes.draw do
 
   resource :contacts, only: [:new, :create], path_names: { :new => '' }
 
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
