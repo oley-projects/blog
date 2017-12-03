32f2cc3e4cc676aa144b750e70d8f65889a80f43
Oley-Projects <oley.for.git@gmail.com>
2017-12-04 00:32:30 +0200

    add factory girl support for rspec

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -5,7 +5,16 @@ describe Article do
     it { should validate_presence_of :title }
     it { should validate_presence_of :text }
   end
+  
   describe "assosiations" do
     it { should have_many :comments }
+  end
+
+  describe "#subject" do
+    it "returns the article title" do
+      article = create(:article, title: 'Lorem Ipsum')
+
+      expect(article.subject).to eq 'Lorem Ipsum'
+    end
   end
 end
