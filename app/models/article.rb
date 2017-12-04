9868474f8fb122be597345157d6de6979dc2c3c9
Oley-Projects <oley.for.git@gmail.com>
2017-12-04 20:48:00 +0200

    Update article validations

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,6 @@
 class Article < ApplicationRecord
-  validates :title, presence: true
-  validates :text, presence: true
+  validates :title, presence: true, length: { in: 2..140 }
+  validates :text, presence: true, length: { in: 2..4000 }
   has_many :comments
 
   def subject
