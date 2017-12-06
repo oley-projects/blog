019766f2126481f45092955576b110a3bc8e99bf
Oley-Projects <oley.for.git@gmail.com>
2017-12-06 13:03:38 +0200

    Article length validation rspec

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -3,7 +3,9 @@ require "spec_helper"
 describe Article do 
   describe "validations" do
     it { should validate_presence_of :title }
+    it { should validate_length_of(:title).is_at_least(3).is_at_most(140) }
     it { should validate_presence_of :text }
+    it { should validate_length_of(:text).is_at_least(3).is_at_most(4000) }
   end
   
   describe "assosiations" do
