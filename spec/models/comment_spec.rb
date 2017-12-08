bf9a0963620d6ba6a5261887041fc4f5c4cb82f4
Oley-Projects <oley.for.git@gmail.com>
2017-12-09 00:18:41 +0200

    Comments rspec

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,12 @@
 require "spec_helper"
 
 describe Comment do 
-  it { should belong_to :article }
+  describe "validations" do
+    it { should validate_presence_of :body }
+    it { should validate_length_of(:body).is_at_least(2).is_at_most(1000) }
+  end
+  
+  describe "assosiations" do
+    it { should belong_to :article }
+  end
 end
