44948c762e33e36c19aa21fd4ffbfc650055e176
Oley-Projects <oley.for.git@gmail.com>
2017-12-09 21:35:27 +0200

    Refactoring. Move signUp to session_helper.rb v2

--- spec/features/visitor_creates_account_spec.rb
+++ spec/features/visitor_creates_account_spec.rb
@@ -6,15 +6,3 @@ feature "Account Creation" do
   end
 
 end
-
-def sign_up
-  visit new_user_registration_path
-
-  fill_in :user_email, :with => 'user@example.com'
-  fill_in :user_username, :with => 'mike'
-  fill_in :user_password, :with => '123456'
-  fill_in :user_password_confirmation, :with => '123456'
-  click_button 'Sign up'
-
-  expect(page).to have_content I18n.t ('devise.registrations.signed_up')
-end
