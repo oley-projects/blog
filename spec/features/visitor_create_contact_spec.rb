e92a704475796dd9e0eff96843c412347c105793
Oley-Projects <oley.for.git@gmail.com>
2017-12-09 18:49:18 +0200

    Create contact acceptance test

---
+++ spec/features/visitor_create_contact_spec.rb
@@ -0,0 +1,9 @@
+require "spec_helper"
+
+feature "Contact Creation" do
+  scenario "allows access to contact page" do
+    visit '/contacts'
+
+    expect(page).to have_content 'Contact us'
+  end
+end
